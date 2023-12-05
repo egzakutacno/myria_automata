34556d91ce6e8427675ef1bc91571088d0144b06
egzakutacno <chakyagm@gmail.com>
2023-12-05 19:11:32 +0800

    automata.sh

--- automata.sh
+++ automata.sh
@@ -9,14 +9,14 @@ if [ -f "$config_file" ]; then
     source "$config_file"
 else
     # Ask the user for API keys and other configurations
-    read -p "Enter Your_Node 1 API: " input_text1
-    read -p "Enter Your_Node 2 API: " input_text2
-    read -p "Enter Your_Node 3 API (press:↵ Enter to skip) : " input_text3
-    read -p "Enter your Telegram Bot Token (press:↵ Enter to continiue without telegram notifications) : " bot_token
-    read -p "Enter your Telegram Chat ID (press:↵ Enter to continiue without telegram notifications): " chat_id
+    read -p "Enter Your Node 1 API: " input_text1
+    read -p "Enter Your Node 2 API: " input_text2
+    read -p "Enter Your Node 3 API (press:↵ Enter to skip): " input_text3
+    read -p "Enter your Telegram Bot Token (press:↵ Enter to continue without telegram notifications): " bot_token
+    read -p "Enter your Telegram Chat ID (press:↵ Enter to continue without telegram notifications): " chat_id
     read -p "Enter the work duration of each node (e.g., 6h 20m): " work_duration
-
-    start_time="08:03"  # Set the desired start time in HH:MM format
+    telegram_url="https://api.telegram.org/bot$bot_token/sendMessage"
+    start_time="12:09"  # Set the desired start time in HH:MM format
     # Set the Telegram URL using the provided bot token
     telegram_url="https://api.telegram.org/bot$bot_token/sendMessage"
 
@@ -36,17 +36,24 @@ fi
 start_command="/usr/local/bin/myria-node --start"
 stop_command="/usr/local/bin/myria-node --stop"
 
-# Function to start the service with input text
+# Function to send a notification with input text
 send_notification() {
     local notification_text="$1"
-    curl -s -X POST $telegram_url -d chat_id=$chat_id -d text="$notification_text"
+    if [ -n "$bot_token" ] && [ -n "$chat_id" ]; then
+        # Assuming you construct the telegram_url using bot_token
+        telegram_url="https://api.telegram.org/bot$bot_token/sendMessage"
+        curl -s -X POST "$telegram_url" -d chat_id="$chat_id" -d text="$notification_text"
+    else
+        echo "Bot token or chat_id is not set. Skipping notification."
+    fi
 }
 
-# Function to start the service with input text
+
+
+#Function to start the service with input text
 start_service() {
     local input_text="$1"
     echo "$input_text" | $start_command
-
 
     # Check if the service started successfully
     if [ $? -eq 0 ]; then
@@ -74,81 +81,104 @@ stop_service() {
     fi
 }
 
+counter=0
 
 while true; do
-    counter=0
     # Get the current time in 24-hour format
     current_time=$(date +%H:%M)
 
     # Calculate the time difference in minutes
     desired_minutes=$(date -d "$start_time" +%s)
     current_minutes=$(date -d "$current_time" +%s)
-    midnight_minutes=$(date -d "tomorrow" +%s)
 
     time_difference=$((desired_minutes - current_minutes))
 
     # Check if the desired time is in the future
     if [ $time_difference -gt 0 ]; then
-        echo "Waiting until $desired_time to run the command..."
+        echo "Waiting until $start_time to run the command..."
         sleep $time_difference
     else
-        # The desired time has already passed; calculate the time until the next day
-        time_until_midnight=$((midnight_minutes - current_minutes))
-        time_until_desired=$((time_until_midnight + desired_minutes))
+        echo "Executing your command at $start_time"
 
-        echo "Waiting until tomorrow $desired_time to run the command..."
-        sleep $time_until_desired
-    fi
+        if [ -n "$input_text1" ]; then
+            ((counter++))
+            echo "The value of API is: $input_text1"
+            start_service "$input_text1"
 
-    # Run your command here
-    echo "Executing your command at $desired_time"
+            # Check if bot_token is set before using curl for Telegram
+            if [ -n "$bot_token" ]; then
+                output=$(/usr/local/bin/myria-node --status)
+                # Format the output for sending via Telegram
+                message=$(echo -e "Command output:\n$output")
+                echo "$message"
+                if [ -n "$chat_id" ]; then
+                    send_notification "$message"
+                else
+                    echo "Telegram chat_id is not set. Skipping notification."
+                fi
+            else
+                echo "Telegram bot_token is not set. Skipping notification."
+            fi
 
-    if [ -n "$input_text1" ]; then
-        ((counter++))
-        echo "The value of API is: $input_text1"
-        start_service $input_text1
-        output=$(/usr/local/bin/myria-node --status)
-        #    Format the output for sending via Telegram
-        message=$(echo -e "Command output:\n$output")
-        echo $message
-        send_notification "$message"
-        sleep $work_duration
-        stop_service $input_text1
-    else
-        echo "Variable1 does not exist."
-    fi
+            sleep $work_duration
+            stop_service "$input_text1"
+        else
+            echo "Variable1 does not exist."
+        fi
 
-    if [ -n "$input_text2" ]; then 
-        ((counter++))
-        echo "The value of API is: $input_text2"
-        start_service $input_text2
-        output=$(/usr/local/bin/myria-node --status)
-        #    Format the output for sending via Telegram
-        message=$(echo -e "Command output:\n$output")
-        echo $message
-        send_notification "$message"
-        sleep $work_duration
-        stop_service $input_text2
-    else
-        echo "Variable2 does not exist."
-    fi
+        if [ -n "$input_text2" ]; then 
+            ((counter++))
+            echo "The value of API is: $input_text2"
+            start_service "$input_text2"
 
-    if [ -n "$input_text3" ]; then
-        ((counter++))
-        echo "The value of API is: $input_text3"
-        start_service $input_text3
-        output=$(/usr/local/bin/myria-node --status)
-        #    Format the output for sending via Telegram
-        message=$(echo -e "Command output:\n$output")
-        echo $message
-        send_notification "$message"
-        sleep $work_duration
-        stop_service $input_text3
-    else
-        echo "Variable3 does not exist."
+            # Check if bot_token is set before using curl for Telegram
+            if [ -n "$bot_token" ]; then
+                output=$(/usr/local/bin/myria-node --status)
+                # Format the output for sending via Telegram
+                message=$(echo -e "Command output:\n$output")
+                echo "$message"
+                if [ -n "$chat_id" ]; then
+                    send_notification "$message"
+                else
+                    echo "Telegram chat_id is not set. Skipping notification."
+                fi
+            else
+                echo "Telegram bot_token is not set. Skipping notification."
+            fi
+
+            sleep $work_duration
+            stop_service "$input_text2"
+        else
+            echo "Variable2 does not exist."
+        fi
+
+        if [ -n "$input_text3" ]; then
+            ((counter++))
+            echo "The value of API is: $input_text3"
+            start_service "$input_text3"
+
+            # Check if bot_token is set before using curl for Telegram
+            if [ -n "$bot_token" ]; then
+                output=$(/usr/local/bin/myria-node --status)
+                # Format the output for sending via Telegram
+                message=$(echo -e "Command output:\n$output")
+                echo "$message"
+                if [ -n "$chat_id" ]; then
+                    send_notification "$message"
+                else
+                    echo "Telegram chat_id is not set. Skipping notification."
+                fi
+            else
+                echo "Telegram bot_token is not set. Skipping notification."
+            fi
+
+            sleep $work_duration
+            stop_service "$input_text3"
+        else
+            echo "Variable3 does not exist."
+        fi
+
+        # Exit the script after running the commands
+        exit 0
     fi
 done
-# Exit the script after completing the loop
-exit 0
-
-
